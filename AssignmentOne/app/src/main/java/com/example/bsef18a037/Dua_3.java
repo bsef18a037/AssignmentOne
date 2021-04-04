7b02842ecd546c027b8756516ce5e14dc41ac844
Moeen Khan <bsef18a037@pucit.edu.pk>
2021-04-04 15:49:45 +0500

    Assignment 1 uploaded

---
+++ AssignmentOne/app/src/main/java/com/example/bsef18a037/Dua_3.java
@@ -0,0 +1,27 @@
+package com.example.bsef18a037;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Dua_3 extends AppCompatActivity {
+    Button playAudio;
+    MediaPlayer mediaPlayer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dua_of_waking_up);
+        getSupportActionBar().setTitle("Dua after waking up from sleep");
+        mediaPlayer= MediaPlayer.create(Dua_3.this,R.raw.duaafterwakingupfromsleep);
+        if(mediaPlayer.isPlaying()==false)
+            mediaPlayer.start();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mediaPlayer.isPlaying()==true)
+            mediaPlayer.stop();
+    }
+}
---
+++ AssignmentOne/app/src/main/java/com/example/bsef18a037/MainMenu.java
@@ -0,0 +1,56 @@
+package com.example.bsef18a037;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainMenu extends AppCompatActivity {
+    Button dua1Button;
+    Button dua2Button;
+    Button dua3Button;
+    Button dua4Button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+        getSupportActionBar().hide();
+        dua1Button=findViewById(R.id.dua1Button);
+        dua2Button=findViewById(R.id.dua2Button);
+        dua3Button=findViewById(R.id.dua3Button);
+        dua4Button=findViewById(R.id.dua4Button);
+        dua1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(getApplicationContext(), Dua_1.class);
+                startActivity(i);
+            }
+        });
+        dua2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(getApplicationContext(), Dua_2.class);
+                startActivity(i);
+            }
+        });
+        dua3Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(), Dua_3.class);
+                startActivity(i);
+            }
+        });
+        dua4Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(new Intent(getApplicationContext(), Dua_4.class));
+                startActivity(i);
+            }
+        });
+
+
+    }
+}
